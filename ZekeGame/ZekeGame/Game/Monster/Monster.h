29e18e6f020293c31c6d5a5bda8c52885883f001
kbc18b21 <kbc18b21@stu.kawahara.ac.jp>
2018-11-06 14:53:06 +0900

    pypy

--- ZekeGame/ZekeGame/Game/Monster/Monster.h
+++ ZekeGame/ZekeGame/Game/Monster/Monster.h
@@ -1,14 +1,37 @@
 #pragma once
-#include "MonsterAI.h"
-class Monster:public MonsterAI
+//#include "MonsterAI.h"
+class MonsterAction;
+class Monster:public GameObject
 {
 public:
-	virtual void execute() final override;
+	void execute();
+
 	enum State
 	{
 		NowLoading,
 		Execute,
 	};
+
+	int GetHP()
+	{
+		return m_HP;
+	}
+	int GetMP()
+	{
+		return m_MP;
+	}
+	CVector3 Getpos()
+	{
+		return m_pos;
+	}
+
 protected:
+	SkinModelRender* m_smr = nullptr;
+	int m_HP = 0;
+	int m_MP = 0;
+	float m_gravity = 50.0f;
+	CVector3 m_pos = CVector3::Zero();
+
+	std::vector<MonsterAction*> m_actions;
 	State m_state = NowLoading;
 };
